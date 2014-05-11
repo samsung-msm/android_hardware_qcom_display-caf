efe295569695bcb5803ff8fb08c85f27458a3fe4
nardholio <nardholio@gmail.com>
2014-05-11 18:37:42 -0400

    Update hwc.cpp

--- libhwcomposer/hwc.cpp
+++ libhwcomposer/hwc.cpp
@@ -152,7 +152,7 @@ static int hwc_prepare_primary(hwc_composer_device_1 *dev,
     hwc_context_t* ctx = (hwc_context_t*)(dev);
     const int dpy = HWC_DISPLAY_PRIMARY;
     int ret = -1;
-    if(UNLIKELY(!ctx->mBasePipeSetup)
+    if(UNLIKELY(!ctx->mBasePipeSetup))
         setupBasePipe(ctx);
     if (LIKELY(list && list->numHwLayers > 1) &&
             ctx->dpyAttr[dpy].isActive) {
